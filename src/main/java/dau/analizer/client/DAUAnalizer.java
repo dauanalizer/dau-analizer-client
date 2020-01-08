71bc5542e77d7db8feda14e219adf8b2555de79e
dauanalizer <dauanalizer@protonmail.com>
2020-01-08 19:43:44 +0300

    add .gitignore file

--- src/main/java/dau/analizer/client/DAUAnalizer.java
+++ src/main/java/dau/analizer/client/DAUAnalizer.java
@@ -1,6 +1,15 @@
 package dau.analizer.client;
 
+import android.app.Activity;
+import android.util.Log;
+
 public class DAUAnalizer {
+
+    private static final String TAG = "DAUAnalizer";
+
+    public void init(Activity activity) {
+        Log.e(TAG, "initialized successfully");
+    }
 
 }
 
